99d855eb3ae2da1c3cf17e7f9aad7bb611790e35
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-09-15 14:03:29 +0000

    Add Items images src

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -5,6 +5,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
+import IMG_SRCS from "./Images"
 
 const Shop = (props) => {
     return (
