35011dedbfc6e3e90ad884173b9daa1bb8909eda
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-09-15 14:00:00 +0000

    Add Container to admin & checkout pages

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,11 +1,17 @@
 import React from "react";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const Admin = (props) => {
     return (
-        <div>
-            <h1>Admin</h1>
-        </div>
+        <Container>
+            <div className="mt-4">
+                <h1>Admin</h1>
+            </div>
+        </Container>
     )
 }
 
--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
 const Checkout = (props) => {
     return (
-        <div>
-            <h1>Checkout</h1>
-        </div>
+        <Container>
+            <div className="mt-4">
+                <h1>Checkout</h1>
+            </div>
+        </Container>
     )
 }
 
