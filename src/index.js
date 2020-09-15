65efec2cd73617ec725eba216f0f62f9d6a0f1f5
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-09-15 14:40:45 +0000

    Provides the store & setup redux devtools

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 
 import "bootswatch/dist/sketchy/bootstrap.min.css"; 
 
+import { Provider } from 'react-redux'
+import store from 'store/store'
+
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
