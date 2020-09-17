d10e59f3066095d141049cb5e048ac5396afe8fd
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-09-17 13:20:50 +0000

    Checkout: UI flow with hooks

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -6,6 +6,7 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import Form from "react-bootstrap/Form";
+import Fade from "react-bootstrap/Fade"
 
 import PRICES from "pages/Shop/Prices"
 import ITEMS from "pages/Shop/Items";
@@ -17,6 +18,14 @@ import { Formik } from "formik";
 // serverside to avoid malicious input.
 
 const Checkout = (props) => {
+    // UI state management with useState to show different checkout steps
+    const [showCart, setShowCart] = useState(true)
+    const [showDelivery, setShowDelivery] = useState(false)
+    const [showPayment, setShowPayment] = useState(false);
+
+    const displayClassName = (show) => {
+        return show ? "" : "d-none"
+    }
     
     let snapCart = {...props.shopCart}
     let shoppingList = <tr><td>you don't have any items in your cart, go to the shop !</td></tr>
@@ -43,12 +52,15 @@ const Checkout = (props) => {
         shoppingList = itemsArray
     }
     
-    // uses useRef to access the address formik field values (I could do it this way too:
-    // pass the values as an argument to the confirmPayment function, since I call it inside
-    //    the onSubmit of the formik form. )
+    let confirmCart = () => {
+        setShowCart(false)
+        setShowDelivery(true)
+    }
+    
+    
+    // uses useRef to access the address formik field values
     let formRef = useRef(null)
     
-
     // send an orderData object to firebase when you confirm the payment
     let orderData = {}
     const confirmPayment = () => {
@@ -61,15 +73,18 @@ const Checkout = (props) => {
                 city: formRef.current.values.city
             }
         }
+        console.log(orderData)
+        console.log("sending it to firebase..")
     }
     return (
         <Container>
             <div className="mt-4">
                 <h1 align="center">Checkout</h1>
                 
-                <h3 align="center">Your shopping cart</h3>
+                <Row className={displayClassName(showCart)}>
                 
-                <Row>
+                    <h3 className="ml-auto mr-auto mt-3">Your shopping cart</h3>
+                
                     <Table striped hover>
                         <thead>
                             <tr>
@@ -86,16 +101,19 @@ const Checkout = (props) => {
 
                     <div className="ml-auto">
                         <Button variant="secondary">Continue Shopping</Button>
-                        <Button variant="success" className="ml-2">Confirm</Button>
+                        <Button variant="success"
+                                className="ml-2"
+                                onClick={confirmCart}>Confirm</Button>
                     </div>
                 </Row>
                 
-                <h3 align="center" className="mt-4">
-                    Your Delivery Information
-                </h3>
-                
-                <Row>
-                    <div className="mt-3 ml-auto mr-auto">
+               
+                <Fade in={showDelivery}>
+                <Row className={displayClassName(showDelivery)}>
+                    <div className="col-md-6 ml-auto mr-auto">
+                        <h3 align="center" className="mt-3">
+                            Your Delivery Information
+                        </h3>
                         <Formik
                            innerRef={formRef}
                            initialValues={{ name: "", street: "", city: ""}}
@@ -119,9 +137,10 @@ const Checkout = (props) => {
                              return errors;
                            }}
                            onSubmit={(values, { setSubmitting }) => {
-                             confirmPayment();
-                             console.log(orderData);
-                             
+                            // hides the delivery information 
+                            // and shows the payment form
+                                setShowDelivery(false);
+                                setShowPayment(true);
                             //  setTimeout(() => {
                             //   alert(JSON.stringify(values, null, 2));
                             //   setSubmitting(false);
@@ -164,73 +183,53 @@ const Checkout = (props) => {
                                     />
                                     {errors.city && touched.city && errors.city}
                                 </Form.Group>
-                             
-                                <Button variant="success">Confirm</Button>
                                 
-                                <h3 align="center" className="mt-4">
-                                        Your Payment Information
-                                    </h3>
-                                <div className="col-md-6 mt-3 ml-auto mr-auto">
-                                    
-                                    
-                                    <h4 className="mt-4">
-                                        Since we sell products for your imagination, we believe
-                                        our profits should be imaginary too !
-                                    </h4>
-                                    <p>wait, what?</p>
-                                    
-                                    
-                                    <Form.Group align="center" controlId="paymentCheckbox" >
-                                        <Form.Check type="checkbox" 
-                                                    label="I'll pay later !"
-                                                    checked 
-                                                    disabled
-                                        />
-                                    </Form.Group>
-                                   
-                                    
-                                   {/* <Button variant="success" 
-                                            className="btn-block"
-                                            onClick={confirmPayment}>Confirm</Button> */}
-                                            
-                                    <Button variant="success" className="btn-block" type="submit" disabled={isSubmitting} >
-                                        Confirm
-                                    </Button>
-                                 </div>
-                                
-                                
-                                
-                                
+                                <Button variant="success"
+                                        className="mr-auto"
+                                        type="submit"
+                                        disabled={isSubmitting} >
+                                    Confirm
+                                </Button>
                              </Form>
-                           )}
+                            )}
                          </Formik>
-                      </div>
-                </Row>
-                
-                
-            { /*    <Row>
-                    <div className="col-6 mt-3 ml-auto mr-auto">
-                        <h4>
-                            Since we sell products for your imagination, we believe
-                            our profits should be imaginary too !
-                        </h4>
-                        <p>wait, what?</p>
-                        
-                        <Form align="center" >
-                            <Form.Group controlId="paymentCheckbox" >
+                    </div>
+                 </Row>
+                 </Fade>
+
+                <Fade in={showPayment}>
+                 <Row>
+                    <div className="col-md-6 ml-auto mr-auto">
+                        <h3 align="center" className="mt-3">
+                                Your Payment Information
+                        </h3>
+                        <div className="mt-3 ml-auto mr-auto">
+                            
+                            
+                            <h4 className="mt-4">
+                                Since we sell products for your imagination, we believe
+                                our profits should be imaginary too !
+                            </h4>
+                            <p>wait, what?</p>
+                            
+                            
+                            <Form.Group align="center" controlId="paymentCheckbox" >
                                 <Form.Check type="checkbox" 
                                             label="I'll pay later !"
                                             checked 
                                             disabled
                                 />
                             </Form.Group>
-                        </Form>
-                        
-                        <Button variant="success" 
-                                className="btn-block"
-                                onClick={confirmPayment}>Confirm</Button>
-                      </div>
-                </Row> */}
+                            
+                            <Button variant="success"
+                                    className="btn-block"
+                                    onClick={confirmPayment}>Confirm</Button>
+                            
+                           
+                         </div>
+                    </div>
+                </Row>
+                </Fade>
             </div>
         </Container>
     )
