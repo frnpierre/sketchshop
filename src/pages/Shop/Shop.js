323ee1a6012900cb9239e95845e3246157a296c7
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-09-18 16:06:59 +0000

    Shop: programmatically generate the item cards

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -2,18 +2,14 @@ import React from "react";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
+
+import ITEMS from "pages/Shop/Items";
 
 import CartButton from "components/CartButton/CartButton";
 import ItemCard from "components/ItemCard/ItemCard";
 import FlashAlert from "components/FlashAlert/FlashAlert";
 
 import { useLocation } from "react-router-dom";
-
-import ITEMS from "pages/Shop/Items";
-
-import * as actions from "store/actions/actions";
 import { connect } from "react-redux";
 
 const Shop = (props) => {
@@ -23,9 +19,20 @@ const Shop = (props) => {
         flash = <FlashAlert show={true}
                             flashType={location.state.flashType}
                             flashMsg={location.state.flashMsg}
-                />
+                />;
     }
 
+    let itemCards = [];
+    for (let item in ITEMS) {
+        itemCards.push(<ItemCard 
+                        // I use the item name as a key since it is unique in this project
+                        key={item}
+                        itemName={item}
+                        itemPrice={ITEMS[item].price}
+                        imgSrc={ITEMS[item].src}
+                     />);
+    }
+    
     return (
         <Container>
             <div className="mt-4">
@@ -38,61 +45,19 @@ const Shop = (props) => {
                     <CartButton />
                 </div>
                 
-{/* The shop items are hardcoded for now. 
-    I realise they should be programmatically generated. */}
                 <Row>
-                    <ItemCard 
-                        itemName="Sketchpad"
-                        itemPrice={ITEMS["Sketchpad"].price}
-                        imgSrc={ITEMS["Sketchpad"].src}
-                     />
-                    <ItemCard 
-                        itemName="Pencil"
-                        itemPrice={ITEMS["Pencil"].price}
-                        imgSrc={ITEMS["Pencil"].src}
-                    />
-                    <ItemCard
-                        itemName="Pencil Box Set"
-                        itemPrice={ITEMS["Pencil Box Set"].price}
-                        imgSrc={ITEMS["Pencil Box Set"].src}
-                    />
-                </Row>
-                
-                <Row>
-                    <ItemCard 
-                        itemName="Electric Eraser"
-                        itemPrice={ITEMS["Electric Eraser"].price}
-                        imgSrc={ITEMS["Electric Eraser"].src}
-                    />
-                    <ItemCard 
-                        itemName="Manikin"
-                        itemPrice={ITEMS["Manikin"].price}
-                        imgSrc={ITEMS["Manikin"].src}
-                    />
-                    <ItemCard
-                        itemName="Vintage Sharpener"
-                        itemPrice={ITEMS["Vintage Sharpener"].price}
-                        imgSrc={ITEMS["Vintage Sharpener"].src} 
-                    />
+                    {itemCards}
                 </Row>
             </div>
         </Container>
-    )
-}
-
+    );
+};
 
 const mapStateToProps = (state) => {
     return {
         shopCart: state.shoppingCart,
         total: state.totalPrice
-    }
-}
-
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         itemAdded: (item) => dispatch(actions.addOneToCart(item)),
-//         itemRemoved: (item) => dispatch(actions.removeOneFromCart(item))
-//     }
-// }
+    };
+};
 
 export default connect(mapStateToProps)(Shop);
