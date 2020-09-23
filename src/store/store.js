7a3c1bdb73559cb3a78f6afcf38add8b564b6677
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-09-23 17:09:35 +0000

    Bugfix: blankpage on load
    The problem seems to be redux dev tools, I removed it from the redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,9 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk"
 import rootReducer from "store/reducers/reducer";
 
+// const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-export default createStore(rootReducer, compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
